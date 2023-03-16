6ad639674f68bf3ec25be9aff34c54db1093d1b2
genyaFiL <genyafil@gmail.com>
2023-03-16 22:06:45 +0200

    fix bugs

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,10 +4,14 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const STORAGE_KEY = 'videoplayer-current-time';
-player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+const videoplayerCurrentTime = localStorage.getItem(STORAGE_KEY);
 
-const currentTime = time => {
+if (videoplayerCurrentTime) {
+  player.setCurrentTime(videoplayerCurrentTime);
+}
+
+function currentTime(time) {
   localStorage.setItem(STORAGE_KEY, `${time.seconds}`);
-};
+}
 
 player.on('timeupdate', throttle(currentTime, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,39 +1,47 @@
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
-
-const feedbackForm = document.querySelector('.feedback-form');
-// console.dir(feedbackForm);
+const feedbackFormEl = document.querySelector('.feedback-form');
 const feedbackEmailInputEl = document.querySelector('input');
 const feedbackMessageInputEl = document.querySelector('textarea');
 
-const form = document.querySelector('.feedback-form');
-
-form.addEventListener('input', throttle(saveDataToLocalStorage, 500));
+feedbackFormEl.addEventListener('input', throttle(saveDataToLocalStorage, 500));
 
 let formData = {};
+
 function saveDataToLocalStorage(event) {
-  formData[event.target.name] = event.target.value;
+  formData = {
+    email: feedbackEmailInputEl.value,
+    message: feedbackMessageInputEl.value,
+  };
+  console.log('event.target.value:', event.target.value);
+  console.log('event.target.name:', event.target.name);
+
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 populateFormData();
-function populateFormData() {
-  const savedSettings = localStorage.getItem(STORAGE_KEY);
-  const parsedSettings = JSON.parse(savedSettings);
 
-  if (parsedSettings.email) {
-    feedbackEmailInputEl.value = parsedSettings.email;
-  }
-  if (parsedSettings.message) {
-    feedbackMessageInputEl.value = parsedSettings.message;
+function populateFormData() {
+  const savedFormData = localStorage.getItem(STORAGE_KEY);
+  const parsedFormData = JSON.parse(savedFormData);
+
+  if (parsedFormData !== null) {
+    feedbackEmailInputEl.value = parsedFormData.email;
+    feedbackMessageInputEl.value = parsedFormData.message;
   }
 }
-form.addEventListener('submit', feedbackFormSubmit);
+
+feedbackFormEl.addEventListener('submit', feedbackFormSubmit);
+
 function feedbackFormSubmit(event) {
   event.preventDefault();
-  console.log('form submitted = ', formData);
 
+  if (!feedbackEmailInputEl.value || !feedbackMessageInputEl.value) {
+    return alert('All fields must be completed!');
+  }
+  console.log('form submitted = ', formData);
+  formData = {};
   event.target.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
